14374c9212f6645df6a63c61070ec20769f6ee79
Clément Richard <clementrichard@macbook-pro-de-clement.home>
2023-12-17 12:25:22 +0100

    Job-Board-finish

--- src/Components/Jobs.jsx
+++ src/Components/Jobs.jsx
@@ -8,54 +8,63 @@ const Jobs = () => {
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"red"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"blue"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"yellow"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"pink"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"green"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"red"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"blue"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"yellow"}
       />
       <Job
         title={"Agent de sécurité Marseille"}
         contractType={"CDI"}
         country={"France"}
         city={"Marseille"}
+        className={"pink"}
       />
     </main>
   );
